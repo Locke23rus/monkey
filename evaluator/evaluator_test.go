d808747bcba20a84d0ef2053968b37af5f0e6dab
Kirill Nikitin <locke23rus@gmail.com>
2024-06-10 22:43:17 +0300

    eval prefix expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -14,6 +14,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -43,6 +45,25 @@ func testEval(input string) object.Object {
 	program := p.ParseProgram()
 
 	return Eval(program)
+}
+
+func TestBabgOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
